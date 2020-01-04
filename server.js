35e18ed8c5ac405d52d718fda4fa99a069a2bca2
Lauren <lauren.c4231@gmail.com>
2020-01-04 17:36:22 +0000

    Set up express server and add documentation comments

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+// Set up our Express server to serve static assets
 const express = require('express');
 const app = express();
+
+/**
+ * Static Server Middleware
+ *
+ * @param {String} root - Public directory / Dist / Src
+ * @param {Object} options
+ */
+
 const fileServerMiddleware = express.static('public');
+
+/**
+ * Mount with 'Use'
+ *
+ * @param {String} Path - Defaults to '/'
+ * @param {Function} Handler
+ */
 
 app.use('/', fileServerMiddleware);
