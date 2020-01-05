32bfc4192593f34128ed7f385df5c544cb8a4bbe
Lauren <lauren.c4231@gmail.com>
2020-01-05 20:36:40 +0000

    Add IssueRow and IssueTable classes and pass example props

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,40 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
     render() {
-        return <div>This is a placeholder for a table of issues</div>;
+        const rowStyle = { border: '1px solid silver', padding: 4 };
+        return (
+            <table style={{ borderCollapse: 'collapse' }}>
+                <thead>
+                    <tr>
+                        <th style={rowStyle}>ID</th>
+                        <th style={rowStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow
+                        rowStyle={rowStyle}
+                        issue_id={1}
+                        issue_title="Error in console when clicking add"
+                    />
+                    <IssueRow
+                        rowStyle={rowStyle}
+                        issue_id={2}
+                        issue_title="Missing bottom border on panelA"
+                    />
+                </tbody>
+            </table>
+        );
+    }
+}
+class IssueRow extends React.Component {
+    render() {
+        const style = this.props.rowStyle;
+        return (
+            <tr>
+                <td style={style}>{this.props.issue_id}</td>
+                <td style={style}>{this.props.issue_title}</td>
+            </tr>
+        );
     }
 }
 
