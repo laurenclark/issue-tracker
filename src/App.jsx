b90a3515eac173d390d6c79e8a1dd6fff619ca08
Lauren <lauren.c4231@gmail.com>
2020-01-05 13:27:52 +0000

    Add babel browser presets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const helloContinents = Array.from(continents, c => `Hello ${c} `);
+const message = helloContinents.join('');
+
 const elem = (
     <div className="myClass" title="Outer Div">
-        <h1>Hello World!</h1>
+        <h1>{message}</h1>
     </div>
 );
 ReactDOM.render(elem, document.getElementById('content'));
