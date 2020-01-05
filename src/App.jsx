3ddbcfaa00eee734afd7ffa1301bad6b0000d318
Lauren <lauren.c4231@gmail.com>
2020-01-05 20:17:10 +0000

    Scaffold issue tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-class HelloWorld extends React.Component {
+class IssueFilter extends React.Component {
     render() {
-        const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-        const helloContinents = Array.from(continents, c => `Hello ${c} `);
-        const message = helloContinents.join('');
+        return <div>This is a placeholder for the issue filter.</div>;
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        return <div>This is a placeholder for a table of issues</div>;
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return <div>This is a placeholder for a form to add a new issue</div>;
+    }
+}
+class IssueList extends React.Component {
+    render() {
         return (
-            <div className="myClass" title="Outer Div">
-                <h1>{message}</h1>
-            </div>
+            <>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </>
         );
     }
 }
 
-const elem = <HelloWorld />;
+const elem = <IssueList />;
 
 ReactDOM.render(elem, document.getElementById('content'));
