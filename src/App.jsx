654a5775837d811a04f83f5b66e0a7609a7a1a30
Lauren <lauren.c4231@gmail.com>
2020-01-05 18:20:44 +0000

    Create React component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const helloContinents = Array.from(continents, c => `Hello ${c} `);
-const message = helloContinents.join('');
+class HelloWorld extends React.Component {
+    render() {
+        const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+        const helloContinents = Array.from(continents, c => `Hello ${c} `);
+        const message = helloContinents.join('');
+        return (
+            <div className="myClass" title="Outer Div">
+                <h1>{message}</h1>
+            </div>
+        );
+    }
+}
 
-const elem = (
-    <div className="myClass" title="Outer Div">
-        <h1>{message}</h1>
-    </div>
-);
+const elem = <HelloWorld />;
+
 ReactDOM.render(elem, document.getElementById('content'));
