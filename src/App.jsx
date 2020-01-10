d480eb5a03d40adcb8199eab1733e5be7730c3b3
Lauren <lauren.c4231@gmail.com>
2020-01-10 20:36:52 +0000

    Map over issues and move styling to index.html head

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,12 @@
-class IssueFilter extends React.Component {
-    render() {
-        return <div>This is a placeholder for the issue filter.</div>;
-    }
-}
-
-class IssueTable extends React.Component {
-    render() {
-        const rowStyle = { border: '1px solid silver', padding: 4 };
-        return (
-            <table style={{ borderCollapse: 'collapse' }} border="4">
-                <thead>
-                    <tr>
-                        <th style={rowStyle}>ID</th>
-                        <th style={rowStyle}>Title</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <IssueRow rowStyle={rowStyle} issue_id={1}>
-                        Error in console when clicking add.
-                    </IssueRow>
-                    <IssueRow rowStyle={rowStyle} issue_id={2}>
-                        <div>
-                            {/*-----------------------------------------------------------------
-
-                            💡  This is passed in as a child of the component with 
-                                props.children - or else how could we style the text with 
-                                <strong> this is part of React's composition model
-
-                                https://reactjs.org/docs/composition-vs-inheritance.html
-
-                            ------------------------------------------------------------------*/}
-                            Missing <strong>bottom</strong> border on panel
-                        </div>
-                    </IssueRow>
-                </tbody>
-            </table>
-        );
-    }
-}
-class IssueRow extends React.Component {
-    render() {
-        const style = this.props.rowStyle;
-        return (
-            <tr>
-                <td style={style}>{this.props.issue_id}</td>
-                {/*-----------------------------------------------------------------
-
-                    💡  We use props.children to pass in styled text or multiple 
-                        This is similar to how slots work in Vue, two different
-                        ways of nesting content in inside the opening and closing 
-                        JSX of a component.
-
-                ------------------------------------------------------------------*/}
-                <td style={style}>{this.props.children}</td>
-            </tr>
-        );
-    }
-}
-
-class IssueAdd extends React.Component {
-    render() {
-        return <div>This is a placeholder for a form to add a new issue</div>;
-    }
-}
-class IssueList extends React.Component {
-    render() {
-        return (
-            <>
-                <h1>Issue Tracker</h1>
-                <IssueFilter />
-                <hr />
-                <IssueTable />
-                <hr />
-                <IssueAdd />
-            </>
-        );
-    }
-}
-
-const elem = <IssueList />;
-
 const issues = [
-    /*--------------------------------------------------------------
-    ## Due left undefined so we can test case for optional fields
-    --------------------------------------------------------------*/
     {
         id: 1,
-        status: 'new',
-        owner: 'Revan',
+        status: 'New',
+        owner: 'Ravan',
         effort: 5,
         created: new Date('2018-08-15'),
         due: undefined,
-        title: 'Error in the console when clicking Add'
+        title: 'Error in console when clicking Add'
     },
     {
         id: 2,
@@ -104,4 +19,75 @@ const issues = [
     }
 ];
 
-ReactDOM.render(elem, document.getElementById('content'));
+class IssueFilter extends React.Component {
+    render() {
+        return <div>This is a placeholder for the issue filter.</div>;
+    }
+}
+
+class IssueRow extends React.Component {
+    render() {
+        const issue = this.props.issue;
+        return (
+            <tr>
+                <td>{issue.id}</td>
+                <td>{issue.status}</td>
+                <td>{issue.owner}</td>
+                <td>{issue.created.toDateString()}</td>
+                <td>{issue.effort}</td>
+                <td>{issue.due ? issue.due.toDateString() : ''}</td>
+                <td>{issue.title}</td>
+            </tr>
+        );
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        const issueRows = issues.map(issue => (
+            <IssueRow key={issue.id} issue={issue} />
+        ));
+
+        return (
+            <table className="bordered-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Status</th>
+                        <th>Owner</th>
+                        <th>Created</th>
+                        <th>Effort</th>
+                        <th>Due Date</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>{issueRows}</tbody>
+            </table>
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return <div>This is a placeholder for a form to add an issue.</div>;
+    }
+}
+
+class IssueList extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </React.Fragment>
+        );
+    }
+}
+
+const element = <IssueList />;
+
+ReactDOM.render(element, document.getElementById('contents'));
