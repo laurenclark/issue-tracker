d82df7300720bec09ec0e6ac7c462d64fb3c4f02
Lauren <lauren.c4231@gmail.com>
2020-01-27 00:59:11 +0000

    Example queries and mutations

--- server/server.js
+++ server/server.js
@@ -3,6 +3,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let aboutMessage = `Issue Tracker API v1.0`;
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage
+    },
+    Mutation: {}
+};
+
+const typeDefs = `
+    type Query {
+        about: String!
+    }
+    type Mutation {
+        setAboutMessage(message: String!) :String;
+    }
+`;
+
+function setAboutMessage(_, { message }) {
+    return (aboutMessage = message);
+}
+
 /**
  * Mount with 'Use'
  *
